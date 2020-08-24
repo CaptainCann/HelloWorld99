04fd99d2a1f75ec74ae251e0d71b673f4b5886fc
CapatainCann <66146222+CapatainCann@users.noreply.github.com>
2020-08-24 12:25:15 -0700

    Initial commit

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView greetingTV;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        greetingTV = (TextView)findViewById(R.id.textView);
+        button = (Button)findViewById(R.id.button);
+
+    }
+
+public void changeExpression(View view){
+
+        if(greetingTV.getText().equals(getResources().getString(R.string.hello)))
+        {
+            greetingTV.setText((R.string.goodbye));
+        }
+        else{
+            greetingTV.setText((R.string.hello));
+        }
+    }
+}
